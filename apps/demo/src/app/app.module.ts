bdea9b1bf00b1b1dd417c9856d3fbaf61f527154
David Smith <davidsmith2@gmail.com>
2024-09-14 19:41:56 -0400

    Wire up @ngrx/store-devtools

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -4,10 +4,12 @@ import {BrowserModule} from '@angular/platform-browser';
 import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
 import {RouterModule} from '@angular/router';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
 import {AppComponent} from './app.component';
 import { DataModule } from './data/data.module';
 import { ngrxEntityRelationshipReducer } from 'ngrx-entity-relationship';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
@@ -16,6 +18,11 @@ import { ngrxEntityRelationshipReducer } from 'ngrx-entity-relationship';
         HttpClientModule,
         StoreModule.forRoot({}, {metaReducers: [ngrxEntityRelationshipReducer]}),
         EffectsModule.forRoot([]),
+        StoreDevtoolsModule.instrument({
+            name: 'ngrx-entity-relationship demo',
+            maxAge: 25,
+            logOnly: environment.production
+        }),
         DataModule,
         RouterModule.forRoot([
             {
